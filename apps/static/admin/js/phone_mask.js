6d64a90df35bd0a9dc78fc1082212f1f91ea2f27
cpcO7 <urunovsamariddin@gmail.com>
2025-02-15 16:12:27 +0500

    added statics

---
+++ apps/static/admin/js/phone_mask.js
@@ -0,0 +1,16 @@
+jQuery(document).ready(function ($) {
+    console.log("Phone mask script loaded.");
+
+    let phoneInput = $("#id_phone_number");
+
+    if (phoneInput.length) {
+        phoneInput.inputmask({
+            mask: "+1 (999) 999-9999",
+            showMaskOnHover: false,
+            showMaskOnFocus: true
+        });
+        console.log("Mask applied successfully.");
+    } else {
+        console.error("Phone input field not found.");
+    }
+});
