6376a1423f08459fde8fa9168af976d32a0eb5f9
cpcO7 <urunovsamariddin@gmail.com>
2025-02-15 15:45:16 +0500

    added postgres

--- root/settings.py
+++ root/settings.py
@@ -106,11 +106,11 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = "static/"
-STATIC_ROOT = os.path.join(BASE_DIR, "apps/static/")
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 
 MEDIA_URL = 'media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
