34bde3cb713c1dd922544cccbbb08c096bd28a7d
cpcO7 <urunovsamariddin@gmail.com>
2025-03-06 08:58:05 +0500

    asd

--- root/urls.py
+++ root/urls.py
@@ -7,6 +7,7 @@ from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 from root.settings import MEDIA_ROOT, MEDIA_URL, STATIC_URL, STATIC_ROOT, ALLOWED_HOSTS
 
 urlpatterns = [
+                  path('admin/', admin.site.urls),
                   path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
                   path('', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
                   path('api/v1/', include("apps.urls")),
